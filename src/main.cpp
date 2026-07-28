f411e80f4996018bee301db726823184ef116aef
MasterChefPhil <philippdevelopmentmail@gmail.com>
2026-07-28 15:39:28 +0200

    wrote updateBuzzer() function

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,31 @@
 #include <Arduino.h>
 
+const uint8_t PIN_ANTENNA = 22;
+const uint8_t PIN_LED_LEVEL_1 = 30;    // LED green 1
+const uint8_t PIN_LED_LEVEL_2 = 31;    // LED green 2
+const uint8_t PIN_LED_LEVEL_3 = 32;    // LED yellow
+const uint8_t PIN_LED_LEVEL_4 = 33;    // LED red 1
+const uint8_t PIN_LED_LEVEL_5 = 34;    // LED red 2
+const uint8_t PIN_BUZZER = 40;         // IN1 driver
+const uint8_t PIN_BUTTON = 44;
+
 bool buzzerEnabled = false;
 bool lastRawButtonState = HIGH;
-bool lastButtonState = HIGH;    // HIGH = not pressed, since INPUT_PULLUP
-
-bool buzzerPinState = false;
-uint32_t lastBuzzerToggle_ms = 0; 
+bool lastButtonState = HIGH;           // HIGH = not pressed, since INPUT_PULLUP
 
 uint32_t lastDebounceTime_ms = 0;
 const uint8_t DEBOUNCE_DELAY_ms = 50;
 
-const uint8_t PIN_ANTENNA = 22;
-const uint8_t PIN_LED1 = 30;    // LED green 1
-const uint8_t PIN_LED2 = 31;    // LED green 2
-const uint8_t PIN_LED3 = 32;    // LED yellow
-const uint8_t PIN_LED4 = 33;    // LED red 1
-const uint8_t PIN_LED5 = 34;    // LED red 2
-const uint8_t PIN_BUZZER = 40;  // IN1 driver
-const uint8_t PIN_BUTTON = 44;
+bool buzzerPinState = false;
+uint32_t lastBuzzerToggle_ms = 0;
+
+const uint32_t ON_TIME = 30;
+const uint32_t OFF_TIME_LEVEL_2 = 400;
+const uint32_t OFF_TIME_LEVEL_3 = 180;
+const uint32_t OFF_TIME_LEVEL_4 = 80;
+const uint32_t OFF_TIME_LEVEL_5 = 30;
+
+uint8_t currentLevel = 0;
 
 // function declarations:
 
@@ -97,8 +105,31 @@ void updateLED(uint32_t now_ms) {
 }
 
 void updateBuzzer(uint32_t now_ms) {
-  if (!buzzerEnabled | fieldStrength == 1) {
+  if (!buzzerEnabled || currentLevel <= 1) {
     digitalWrite(PIN_BUZZER, HIGH);
     return;
+  }
+
+  uint32_t onTime = ON_TIME;
+  uint32_t offTime;
+
+  if (currentLevel == 2) {
+    offTime = OFF_TIME_LEVEL_2;
+  } else if (currentLevel == 3) {
+    offTime = OFF_TIME_LEVEL_3;
+  } else if (currentLevel == 4) {
+    offTime = OFF_TIME_LEVEL_4;
+  } else if (currentLevel == 5) {
+    offTime = OFF_TIME_LEVEL_5;
+  } else {
+    offTime = OFF_TIME_LEVEL_2;
+  }
+
+  uint32_t threshold = buzzerPinState ? onTime : offTime;
+
+  if ((now_ms - lastBuzzerToggle_ms) >= threshold) {
+    buzzerPinState = !buzzerPinState;
+    digitalWrite(PIN_BUZZER, buzzerPinState ? LOW : HIGH);
+    lastBuzzerToggle_ms = now_ms;
   }
 }
